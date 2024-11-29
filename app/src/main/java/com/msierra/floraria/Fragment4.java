35b9ed8a8897e11f4af7608f6f486ff13a0a1e85
Bruno Veiga <veiga.munoz@gmail.com>
2024-11-29 17:24:51 +0100

    Añadida funcionalidad al cardview 4

---
+++ app/src/main/java/com/msierra/floraria/Fragment4.java
@@ -0,0 +1,32 @@
+package com.msierra.floraria;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Fragment4 extends Fragment {
+
+    public Fragment4() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_4, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView txt1 = view.findViewById()
+    }
+}
