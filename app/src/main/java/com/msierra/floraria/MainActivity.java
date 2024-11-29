709ecfcc31a77b63bc7d34b72bb6b69fb761a8b5
KVinny <kevinluislaurentelopez@gmail.com>
2024-11-29 14:02:36 +0100

    Dale

--- app/src/main/java/com/msierra/floraria/MainActivity.java
+++ app/src/main/java/com/msierra/floraria/MainActivity.java
@@ -1,14 +1,22 @@
 package com.msierra.floraria;
 
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView carta3;
+    LinearLayout hiddenView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        carta3 = findViewById(R.id.carta3);
+        hiddenView = findViewById(R.id.hidden_view);
+
+        carta3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if (hiddenView.getVisibility() == View.VISIBLE) {
+                    // The transition of the hiddenView is carried out by the TransitionManager class.
+                    // Here we use an object of the AutoTransition Class to create a default transition
+                    TransitionManager.beginDelayedTransition(carta3, new AutoTransition());
+                    hiddenView.setVisibility(View.GONE);
+
+                }
+
+                // If the CardView is not expanded, set its visibility to
+                // visible and change the expand more icon to expand less.
+                else {
+                    TransitionManager.beginDelayedTransition(carta3, new AutoTransition());
+                    hiddenView.setVisibility(View.VISIBLE);
+                    for (int i = 0; i < carta3.getChildCount(); i++) {
+                        View child = carta3.getChildAt(i);
+                        child.setVisibility(View.INVISIBLE);
+                    }
+                }
+            }
+        });
+
     }
 }
