7067ea5282d4cb38a9a0f5cc047271b0bed6b9ba
anahi hinojosa <ana11hinograje@gmail.com>
2024-11-29 14:02:12 +0100

    Anahi

--- app/src/main/java/com/msierra/floraria/MainActivity.java
+++ app/src/main/java/com/msierra/floraria/MainActivity.java
@@ -1,20 +1,32 @@
 package com.msierra.floraria;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CardView flor2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        flor2 = findViewById(R.id.carta2);
+
+        flor2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
