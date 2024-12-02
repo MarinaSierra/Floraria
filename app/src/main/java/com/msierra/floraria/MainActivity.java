88bd74a8d4fd88744a11efd88deb11be37df38f8
anahi.hinojosa <ana11hinograje@gmail.com>
2024-12-02 19:27:46 +0100

    ver error

--- app/src/main/java/com/msierra/floraria/MainActivity.java
+++ app/src/main/java/com/msierra/floraria/MainActivity.java
@@ -1,7 +1,10 @@
 package com.msierra.floraria;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private CardView flor2;
+    private FrameLayout frame2;
+    Boolean visible = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +24,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         flor2 = findViewById(R.id.carta2);
-
+        frame2 = findViewById(R.id.frame);
+        frame2.setVisibility(View.INVISIBLE);
         flor2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction =  fragmentManager.beginTransaction();
+                FlorFragment2 flor  = new FlorFragment2();
+                transaction.add(R.id.frame,flor);
+                transaction.commit();
+
+                if(!visible){
+                            frame2.setVisibility(View.VISIBLE);
+
+
+
+                             visible=true;
+                        }else{
+                            frame2.setVisibility(View.INVISIBLE);
+                            visible=false;
+                        }
 
             }
         });
